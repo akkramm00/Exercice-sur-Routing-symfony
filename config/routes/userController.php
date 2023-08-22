55b89d55f45df5590f0a7a9e82fb18e1f66f9489
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-08-22 20:55:29 +0000

    add all modif

--- config/routes/userController.php
+++ config/routes/userController.php
@@ -3,14 +3,14 @@ namespace App\Controller
 
   use Symfony\Component\Routing\Annotation\Route
 
-#[Route('/users', name: "user_liost", methods: ['GET'])]
+#[Route('/users', name: "user_list", methods: ['GET'])]
   public function listUsers (userRepository $userRepo)
   {
     //...
   }
 
 #[Route('/users/create', name: 'user_create', methods: ['GET', 'POST'])]
-  public function createUsers(Request $request, EntityManagerInterface $em, UserPossworHasherInterface)
+  public function createUsers(Request $request, EntityManagerInterface $em, UserPassworHasherInterface)
   {
     //...
   }
